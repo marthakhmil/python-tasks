62f78b780f0b90ea8067f8b005a96cac462e20a8
Martha Khmil <martha.khmilgmail.com>
2018-11-22 19:48:25 +0200

    add first home task

---
+++ task-1.py
@@ -0,0 +1,5 @@
+name = input("what is your name?")
+age = input("how old are you?")
+place_of_livig = input("where do you live?")
+total_info =" Hellow,"+ name + "\n Your age is " +str(age) + "\n You live in " + place_of_livig
+print(total_info)
