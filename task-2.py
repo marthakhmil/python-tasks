f337078d7d248ac9eae5d6de4821cb5d8464ab98
Martha Khmil <martha.khmilgmail.com>
2018-11-22 19:51:39 +0200

    add second home task

---
+++ task-2.py
@@ -0,0 +1,11 @@
+a=int(input("enter a = "))
+b=int(input("enter b = "))
+plus=a+b
+minus=b-a
+multiply=a*b
+power=pow(a,b)
+divide=a/b
+divide_integer=a//b
+vestige=a%b
+print(" a+b=", plus, "\n b-a=",minus, "\n a*b=",multiply, "\n a^b",power, "\n a/b=", divide, "\n a//b=", divide_integer, "\n a%b= ",vestige)
+
