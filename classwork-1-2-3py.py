34ccff3eb24e63c02d8938ff06e7c5c779150c7a
martha khmil <martha.khmil@gmail.com>
2018-11-26 20:25:06 +0200

    add my classwork to repository

---
+++ classwork-1-2-3py.py
@@ -0,0 +1,22 @@
+a=int(input('a='))
+b=int(input('b='))
+if a>b:print('number a is bigger then b')
+elif a<b:print('number b is bigger then a')
+else: ('numbers are equal')
+
+c=int(input('c='))
+if (c%2)==0:print('c is even number ')
+else:print('this number is the odd one')
+
+n=int(input('n='))
+i = 1
+factorial = 1
+while i <= n:
+    factorial *= i
+    i += 1
+print("n!",n, "=", factorial)
+
+
+
+
+
