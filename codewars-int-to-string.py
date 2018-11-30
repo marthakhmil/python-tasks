94b3db8b8a30a268909011315da59250393ff538
martha khmil <martha.khmil@gmail.com>
2018-11-30 14:43:22 +0200

    add codewars 3 tasks

---
+++ codewars-int-to-string.py
@@ -0,0 +1,3 @@
+def number_to_string(num):
+    result=str(num)
+    return result
---
+++ codewars-reverse.py
@@ -0,0 +1,5 @@
+def reverse(s):
+    s_splited=s.split(" ")
+    s_splited=s_splited[::-1]
+    st=" ".join(s_splited)
+    return st
