cc8ee1e486e090399ffdcb06777960852dd0b0cb
martha khmil <martha.khmil@gmail.com>
2018-11-30 14:53:41 +0200

    create a folder for classwork

--- classwork-5-str.py
+++
@@ -1,5 +0,0 @@
-list_str=['dodo','roro','momo','soso']
-for i in list_str:
-    print(i)
-
-
--- classwork-6-fibonacci.py
+++
@@ -1,7 +0,0 @@
-n=int(input('enter some number from fibonacci list='))
-fibonacci_numbers = [0, 1]
-for i in range(2,n):
-    fibonacci_numbers.append(fibonacci_numbers[i-1]+fibonacci_numbers[i-2])
-    if fibonacci_numbers[i]==n:
-        break
-print('there are some numbers from fibanacci till that you entered',n,'\n',fibonacci_numbers)
